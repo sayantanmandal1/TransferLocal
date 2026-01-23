7295ce9921237931290fc4c10b8b358d6a809281
Sayantan Mandal <msayantan05@gmail.com>
2026-01-23 10:32:15 +0530

    fix(hotspot): add timeout for DNS resolution (revise approach)

--- server/hotspot.js
+++ server/hotspot.js
@@ -27,7 +27,7 @@ function getNetworkStatus() {
       return;
     }
 
-    dns.resolve('dns.google', { timeout: 3000 }, (err) => {
+    dns.resolve('dns.google', (err) => {
       if (err) {
         resolve('local');
       } else {
