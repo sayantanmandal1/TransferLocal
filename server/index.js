70df2a98220b25db82b4e41dc90def957f4408fc
Sayantan Mandal <msayantan05@gmail.com>
2026-04-19 10:15:40 +0530

    fix(server): add cache-control header to API responses

--- server/index.js
+++ server/index.js
@@ -51,6 +51,7 @@ app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('Referrer-Policy', 'no-referrer');
+  res.setHeader('Cache-Control', 'no-store');
   next();
 });
 
