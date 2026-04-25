591c368f9d826654ccac10d173e9738f48806511
Sayantan Mandal <msayantan05@gmail.com>
2026-04-25 13:40:27 +0530

    chore(identity): add comment for hostname validation (revise approach)

--- server/identity.js
+++ server/identity.js
@@ -9,7 +9,6 @@ const CONFIG_PATH = path.join(__dirname, 'config.json');
 
 let identity = null;
 
-// Loads or creates device identity, regenerating if copied from another machine
 function loadIdentity() {
   if (identity) return identity;
 
